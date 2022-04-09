b4c5c9830c2eeb5cef2406ce5551e75405bb228b
dinodave88 <dinodave88@gmail.com>
2022-04-09 21:03:41 +0530

    updated host detail

--- python_django/settings.py
+++ python_django/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_0b7kbv!-b_krpr6i!naz9=6x=g_kc=@x5t(+-e7#l8pi63*zx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
